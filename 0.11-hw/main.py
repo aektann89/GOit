5e663269c2ddaa332fc50ac9706d9a6ba55efc32
chaek <kuryachiy.andrey.sergeevich@gmail.com>
2023-11-28 15:52:37 +0200

    update

--- 0.11-hw/main.py
+++ 0.11-hw/main.py
@@ -1,6 +1,8 @@
 import re
-from collections import UserDict
 from datetime import datetime, timedelta
+from collections import UserDict
+
+
 class Field:
     def __init__(self, value):
         self._value = None
@@ -9,17 +11,16 @@ class Field:
     @property
     def value(self):
         return self._value
-    
+
     @value.setter
     def value(self, new_value):
-        self.validate(new_value)
         self._value = new_value
 
-    
 
 class Name(Field):
     def __init__(self, value):
         super().__init__(value)
+
 
 class Phone(Field):
     def __init__(self, value):
@@ -32,14 +33,12 @@ class Phone(Field):
         self.validate_phone()
 
     def validate_phone(self):
-        # Валідація телефонного номера (10 цифр)
+        # Валідація номера телефону (10 цифр)
         pattern = re.compile(r'^\d{10}$')
         if not pattern.match(self.value):
             raise ValueError("Неправильний формат номера телефону")
-        
-    def get_value(self):
-        return f"Phone: {self.value}"
-        
+
+
 class Birthday(Field):
     def __init__(self, value):
         super().__init__(value)
@@ -56,9 +55,7 @@ class Birthday(Field):
             datetime.strptime(self.value, '%Y-%m-%d')
         except ValueError:
             raise ValueError("Неправильний формат дня народження")
-        
-    def get_value(self):
-        return f"Birthday: {self.value}"
+
 
 class Record:
     def __init__(self, name_value, birthday_value=None):
@@ -73,20 +70,18 @@ class Record:
         self.phones.append(phone)
 
     def remove_phone(self, phone_value):
-        # Використовуємо filter для створення нового списку без вказаного номера телефону
-        self.phones = list(filter(lambda phone: phone.value != phone_value, self.phones))
+        self.phones = [phone for phone in self.phones if phone.value != phone_value]
 
     def edit_phone(self, old_phone_value, new_phone_value):
         for phone in self.phones:
             if phone.value == old_phone_value:
                 phone.value = new_phone_value
-                return  # Додано return, щоб зупинити пошук після редагування першого номера
-        # Якщо немає відповідного номера телефону, викидаємо ValueError
+                return
         raise ValueError("Номер телефону не існує")
 
     def find_phone(self, phone_value):
         return next((phone for phone in self.phones if phone.value == phone_value), None)
-    
+
     def days_to_birthday(self):
         if not self.birthday:
             return None
@@ -95,16 +90,7 @@ class Record:
         if today > next_birthday:
             next_birthday = datetime(today.year + 1, self.birthday.value.month, self.birthday.value.day)
         return (next_birthday - today).days
-    
-    def get_value(self):
-        result = [f"Name: {self.name.get_value()}"]
-        if self.phones:
-            result.append("Phones:")
-            for phone in self.phones:
-                result.append(f"  {phone.get_value()}")
-        if self.birthday:
-            result.append(f"Birthday: {self.birthday.get_value()}")
-        return "\n".join(result)
+
 
 class AddressBook(UserDict):
     def __init__(self):
