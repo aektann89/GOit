eda1a4597943fbf6e689121811fc1584580398a7
chaek <kuryachiy.andrey.sergeevich@gmail.com>
2023-11-29 19:00:53 +0200

    uuff

--- 0.11-hw/main.py
+++ 0.11-hw/main.py
@@ -1,113 +1,108 @@
-import re
-from datetime import datetime, timedelta
 from collections import UserDict
+from datetime import datetime
+
 
 
 class Field:
     def __init__(self, value):
-        self._value = None
         self.value = value
 
     @property
     def value(self):
         return self._value
-
+    
     @value.setter
     def value(self, new_value):
         self._value = new_value
 
+    def __str__(self):
+        return str(self.value)
 
 class Name(Field):
-    def __init__(self, value):
-        super().__init__(value)
-
+    pass
 
 class Phone(Field):
     def __init__(self, value):
+        if not isinstance(value, str) or not value.isdigit() or len(value) != 10:
+            raise ValueError("Invalid phone number format")
         super().__init__(value)
-        self.validate_phone()
 
     @Field.value.setter
     def value(self, new_value):
-        super().value = new_value
-        self.validate_phone()
-
-    def validate_phone(self):
-        # Валідація номера телефону (10 цифр)
-        pattern = re.compile(r'^\d{10}$')
-        if not pattern.match(self.value):
-            raise ValueError("Неправильний формат номера телефону")
-
+        if not isinstance(new_value, str) or not new_value.isdigit() or len(new_value) != 10:
+            raise ValueError("Invalid phone number format")
+        self._value = new_value
 
 class Birthday(Field):
     def __init__(self, value):
+        try:
+            datetime.strptime(value, '%Y-%m-%d')
+        except ValueError:
+            raise ValueError('Invalid birthday format, use YYYY-MM-DD')
+        
         super().__init__(value)
-        self.validate_birthday()
 
     @Field.value.setter
     def value(self, new_value):
-        super().value = new_value
-        self.validate_birthday()
-
-    def validate_birthday(self):
-        # Валідація формату дня народження (YYYY-MM-DD)
-        try:
-            datetime.strptime(self.value, '%Y-%m-%d')
-        except ValueError:
-            raise ValueError("Неправильний формат дня народження")
-
+        self._value = new_value
 
 class Record:
-    def __init__(self, name_value, birthday_value=None):
-        self.name = Name(name_value)
+    def __init__(self, name, birthday=None):
+        super().__init__(name)
+        self.name = Name(name)
         self.phones = []
-        self.birthday = None
-        if birthday_value:
-            self.birthday = Birthday(birthday_value)
+        self.birthday = Birthday(birthday) if birthday else None
 
-    def add_phone(self, phone_value):
-        phone = Phone(phone_value)
-        self.phones.append(phone)
+    def add_phone(self, phone):
+        self.phones.append(Phone(phone))
 
-    def remove_phone(self, phone_value):
-        self.phones = [phone for phone in self.phones if phone.value != phone_value]
+    def remove_phone(self, phone):
+        self.phones = [p for p in self.phones if p.value != phone]
 
-    def edit_phone(self, old_phone_value, new_phone_value):
-        for phone in self.phones:
-            if phone.value == old_phone_value:
-                phone.value = new_phone_value
-                return
-        raise ValueError("Номер телефону не існує")
+    def edit_phone(self, old_phone, new_phone):
+        found_phone = self.find_phone(old_phone)
+        if found_phone:
+            found_phone.value = new_phone
+        else:
+            raise ValueError(f"Phone number {old_phone} not found in record")
 
-    def find_phone(self, phone_value):
-        return next((phone for phone in self.phones if phone.value == phone_value), None)
-
+    def find_phone(self, phone):
+        for p in self.phones:
+            if p.value == phone:
+                return p
+        return None
+    
     def days_to_birthday(self):
         if not self.birthday:
             return None
-        today = datetime.now()
-        next_birthday = datetime(today.year, self.birthday.value.month, self.birthday.value.day)
+        today = datetime.now().date()
+        next_birthday = datetime(today.year, self.birthday.value.month, self.birthday.value.day).date()
         if today > next_birthday:
-            next_birthday = datetime(today.year + 1, self.birthday.value.month, self.birthday.value.day)
+            next_birthday = datetime(today.year + 1, self.birthday.value.month, self.birthday.value.day).date()
         return (next_birthday - today).days
 
+    def __str__(self):
+        phones_str = '; '.join(str(p) for p in self.phones)
+        return f"Contact name: {self.name.value}, phones: {phones_str}"
 
 class AddressBook(UserDict):
-    def __init__(self):
-        super().__init__()
-
     def add_record(self, record):
         self.data[record.name.value] = record
 
-    def find(self, name_value):
-        return self.data.get(name_value)
+    def find(self, name):
+        return self.data.get(name)
 
-    def delete(self, name_value):
-        if name_value in self.data:
-            del self.data[name_value]
+    def delete(self, name):
+        if name in self.data:
+            del self.data[name]
 
-    def iterator(self, n=1):
-        # Генератор, що повертає уявлення для N записів
-        records = list(self.data.values())
-        for i in range(0, len(records), n):
-            yield records[i:i + n]
+    def iterator(self, item_number):
+        count = 0
+        result = ''
+        for item, record in self.data.items():
+            result += f'{item}: {record}'
+            count += 1
+            if count >= item_number:
+                yield result
+                count = 0
+                result = ''
